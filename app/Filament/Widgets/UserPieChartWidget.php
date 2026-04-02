9b919af7c3e8dc0cdc9be59805ab49f61943eeeb
Habinan27 <habihabinan0@gmail.com>
2026-04-02 14:55:29 +0530

    Add user chart widgets and trend dependency
    Add two Filament chart widgets: UserChartWidget (line) which uses Flowframe\Trend to count daily new users for the current month, and UserPieChartWidget (pie) with static sample data. Also update composer.json to require flowframe/laravel-trend so trend queries are available for the dashboard.

---
+++ app/Filament/Widgets/UserPieChartWidget.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+
+class UserPieChartWidget extends ChartWidget
+{
+    protected ?string $heading = 'User Pie Chart Widget';
+
+    //protected ?string $maxHeight = '400px';
+
+    protected function getData(): array
+    {
+        return [
+            'datasets' => [
+                [
+                    'label' => 'users created',
+                    'data' => [60, 10, 56, 22],
+                    'backgroundColor' => [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)',
+                        'rgb(7, 255, 44)',
+                    ],
+                ],
+            ],
+            'labels' => ['Srilanka', 'India', 'USA', 'Canada',],
+
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'pie';
+    }
+}
